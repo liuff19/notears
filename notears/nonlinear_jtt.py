05eda880616d2ba53ed4b69d0b30818d9ddf3c2e
liuff19 <1447243578@qq.com>
2022-06-22 23:58:19 +0800

    add some jtt codes

--- notears/nonlinear_jtt.py
+++ notears/nonlinear_jtt.py
@@ -11,24 +11,26 @@ import math
 import tqdm as tqdm
 from runhelper import *
 from loss_func import *
+
+
 class NotearsMLP(nn.Module):
     def __init__(self, dims, bias=True):
         super(NotearsMLP, self).__init__()
-        assert len(dims) >= 2    
+        assert len(dims) >= 2
         assert dims[-1] == 1
         d = dims[0]
         self.dims = dims
         # fc1: variable splitting for l1
-        self.fc1_pos = nn.Linear(d, d * dims[1], bias=bias)  
+        self.fc1_pos = nn.Linear(d, d * dims[1], bias=bias)
         self.fc1_neg = nn.Linear(d, d * dims[1], bias=bias)
         self.fc1_pos.weight.bounds = self._bounds()
         self.fc1_neg.weight.bounds = self._bounds()
         # fc2: local linear layers
         layers = []
         for l in range(len(dims) - 2):
-            layers.append(LocallyConnected(d, dims[l + 1], dims[l + 2], bias=bias))
-        self.fc2 = nn.ModuleList(layers) 
-        
+            layers.append(LocallyConnected(
+                d, dims[l + 1], dims[l + 2], bias=bias))
+        self.fc2 = nn.ModuleList(layers)
 
     def _bounds(self):
         d = self.dims[0]
@@ -44,18 +46,18 @@ class NotearsMLP(nn.Module):
         return bounds
 
     def forward(self, x):  # [n, d] -> [n, d]
-        x = self.fc1_pos(x) - self.fc1_neg(x)  # [n, d * m1] 
+        x = self.fc1_pos(x) - self.fc1_neg(x)  # [n, d * m1]
         x = x.view(-1, self.dims[0], self.dims[1])  # [n, d, m1]
         for fc in self.fc2:
             x = torch.sigmoid(x)  # [n, d, m1]
             x = fc(x)  # [n, d, m2]
-        x = x.squeeze(dim=2)  # [n, d] 
+        x = x.squeeze(dim=2)  # [n, d]
         return x
 
     def h_func(self):
         """Constrain 2-norm-squared of fc1 weights along m1 dim to be a DAG"""
         d = self.dims[0]
-        fc1_weight = self.fc1_pos.weight - self.fc1_neg.weight  # [j * m1, i] 
+        fc1_weight = self.fc1_pos.weight - self.fc1_neg.weight  # [j * m1, i]
         fc1_weight = fc1_weight.view(d, -1, d)  # [j, m1, i]
         A = torch.sum(fc1_weight * fc1_weight, dim=1).t()  # [i, j]
         h = trace_expm(A) - d  # (Zheng et al. 2018)
@@ -72,7 +74,7 @@ class NotearsMLP(nn.Module):
         reg += torch.sum(fc1_weight ** 2)
         for fc in self.fc2:
             reg += torch.sum(fc.weight ** 2)
-        return reg 
+        return reg
 
     def fc1_l1_reg(self):
         """Take l1 norm of fc1 weight"""
@@ -162,7 +164,6 @@ class NotearsSobolev(nn.Module):
         return W
 
 
-
 def dual_ascent_step(model, X, lambda1, lambda2, rho, alpha, h, rho_max):
     """Perform one step of dual ascent in augmented Lagrangian."""
     h_new = None
@@ -209,6 +210,66 @@ def notears_nonlinear(model: nn.Module,
     W_est[np.abs(W_est) < w_threshold] = 0
     return W_est
 
+def dual_ascent_step_reweight(model, X, lambda1, lambda2, rho, alpha, h, rho_max, reweight_list = None, beta=1):
+    """Perform one step of dual ascent in augmented Lagrangian."""
+    h_new = None
+    optimizer = LBFGSBScipy(model.parameters())
+    X_torch = torch.from_numpy(X)
+    while rho < rho_max:
+        def closure():
+            optimizer.zero_grad()
+            X_hat = model(X_torch)
+            loss = reweighted_loss(X_hat, X_torch, beta=beta, reweight_list=reweight_list)
+            h_val = model.h_func()
+            penalty = 0.5 * rho * h_val * h_val + alpha * h_val
+            l2_reg = 0.5 * lambda2 * model.l2_reg()
+            l1_reg = lambda1 * model.fc1_l1_reg()
+            primal_obj = loss + penalty + l2_reg + l1_reg
+            primal_obj.backward()
+            return primal_obj
+        optimizer.step(closure)  # NOTE: updates model in-place
+        with torch.no_grad():
+            h_new = model.h_func().item()
+        if h_new > 0.25 * h:
+            rho *= 10
+        else:
+            break
+    alpha += rho * h_new
+    return rho, alpha, h_new
+
+def notears_nonlinear_reweight(model: nn.Module,
+                          X: np.ndarray,
+                          lambda1: float = 0.,
+                          lambda2: float = 0.,
+                          max_iter: int = 100,
+                          h_tol: float = 1e-8,
+                          rho_max: float = 1e+16,
+                          w_threshold: float = 0.3,
+                          beta = 1,
+                          reweight_epoch=100):
+
+    rho, alpha, h = 1.0, 0.0, np.inf
+    reweight_list = []
+    for epoch in tqdm.tqdm(range(max_iter)):
+        if epoch < reweight_epoch:
+            rho, alpha, h = dual_ascent_step(model, X, lambda1, lambda2,
+                                         rho, alpha, h, rho_max)
+        elif epoch == reweight_epoch:
+            X_torch = torch.from_numpy(X)
+            loss_record = single_loss(model(X_torch), X_torch)
+            each_loss = torch.sum(loss_record, dim=1)
+            each_loss = each_loss.cpu().detach().numpy()
+            each_loss_idx = each_loss.argsort()
+            reweight_list = each_loss_idx[:int(len(each_loss_idx) * 0.1)]
+            rho, alpha, h = dual_ascent_step(model, X, lambda1, lambda2, rho, alpha, h, rho_max)
+        else:
+            rho, alpha, h = dual_ascent_step_reweight(model, X, lambda1, lambda2, rho, alpha, h, rho_max, reweight_list=reweight_list, beta = beta)
+        if h <= h_tol or rho >= rho_max:
+            break
+    W_est = model.fc1_to_adj()
+    W_est[np.abs(W_est) < w_threshold] = 0
+    return W_est
+
 
 def main():
     # fangfu
@@ -218,10 +279,10 @@ def main():
     print('==' * 20)
 
     torch.set_default_dtype(torch.double)
-    np.set_printoptions(precision=3) 
+    np.set_printoptions(precision=3)
 
     import utils as ut
-    ut.set_random_seed(123)
+    ut.set_random_seed(args.seed)
 
     B_true = ut.simulate_dag(args.d, args.s0, args.graph_type)
     # np.savetxt('W_true.csv', B_true, delimiter=',')
@@ -230,13 +291,20 @@ def main():
     # np.savetxt('X.csv', X, delimiter=',')
 
     model = NotearsMLP(dims=[args.d, 10, 1], bias=True)
+
     W_est = notears_nonlinear(model, X, args.lambda1, args.lambda2)
     assert ut.is_dag(W_est)
     # np.savetxt('W_est.csv', W_est, delimiter=',')
     acc = ut.count_accuracy(B_true, W_est != 0)
-    print(acc)
+    print(f'the result w/o reweighting: {acc}')
+
     if args.reweight:
         print('reweighting')
+        W_est = notears_nonlinear_reweight(model, X, args.lambda1, args.lambda2, beta = args.beta, reweight_epoch=args.reweight_epoch)
+        assert ut.is_dag(W_est)
+        acc_jtt = ut.count_accuracy(B_true, W_est != 0)
+        print(f'the result w/ reweighting: {acc_jtt}')
+
 
 if __name__ == '__main__':
     main()
--- notears/runhelper.py
+++ notears/runhelper.py
@@ -5,6 +5,7 @@ def config_parser():
 
     parser.add_argument('--config', is_config_file=True,help='config file path')
     parser.add_argument('--data_dir', type=str, default='data', help='dataset_path')
+    parser.add_argument('--seed', type=int, default=123, help='random seed')
     parser.add_argument('--d', type=int, default=5, help='number of nodes')
     parser.add_argument('--s0', type=int, default=9, help='number of edges')
     parser.add_argument('--graph_type', type=str, default='ER', help='graph type')
@@ -12,5 +13,7 @@ def config_parser():
     parser.add_argument('--n', type=int, default=200, help='number of samples')
     parser.add_argument('--lambda1', type=float, default=0.01, help='lambda1')
     parser.add_argument('--lambda2', type=float, default=0.01, help='lambda2')
-
+    parser.add_argument('--reweight', action='store_true', help='if reweight')
+    parser.add_argument('--beta', type=float, default=0.9, help='beta')
+    parser.add_argument('--reweight_epoch', type=int, default=3, help='the epoch begin to reweight')
     return parser
