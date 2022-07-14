3c351a7d85bc63a9446064d4adfe63c6231ce58a
liuff19 <1447243578@qq.com>
2022-07-14 14:33:53 +0800

    runhelper.py change args

--- notears/runhelper.py
+++ notears/runhelper.py
@@ -6,15 +6,15 @@ def config_parser():
     parser.add_argument('--config', is_config_file=True,help='config file path')
     parser.add_argument('--device', default='cpu', help='cuda or cpu')
 
-    parser.add_argument("--s0", default=40, type=int)
-    parser.add_argument("--d", default=10, type=int)
+    parser.add_argument("--s0", default=100, type=int)
+    parser.add_argument("--d", default=25, type=int)
     parser.add_argument("--n", default=1000, type=int)
     parser.add_argument("--sem_type", default="mlp", choices=["gp-add","mlp", "gp", "mim"])
     parser.add_argument("--graph_type", default='SF')
 
 
     parser.add_argument('--data_dir', type=str, default='data', help='dataset_path')
-    parser.add_argument('--seed', type=int, default=123, help='random seed')
+    parser.add_argument('--seed', type=int, default=1, help='random seed')
 
     parser.add_argument('--lambda1', type=float, default=0.01, help='lambda1')
     parser.add_argument('--lambda2', type=float, default=0.01, help='lambda2')
