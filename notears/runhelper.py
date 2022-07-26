ee0c0552eae81058dc57d681fcf8ebedbde795df
liuff19 <1447243578@qq.com>
2022-07-26 23:50:48 +0800

    add the batchsize to runhelper

--- notears/runhelper.py
+++ notears/runhelper.py
@@ -6,10 +6,10 @@ def config_parser():
     parser.add_argument('--config', is_config_file=True,help='config file path')
     parser.add_argument('--device', default='cpu', help='cuda or cpu')
 
-    parser.add_argument("--s0", default=80, type=int)
-    parser.add_argument("--d", default=20, type=int)
+    parser.add_argument("--s0", default=40, type=int)
+    parser.add_argument("--d", default=10, type=int)
     parser.add_argument("--n", default=1000, type=int)
-    parser.add_argument("--sem_type", default="mlp", choices=["gp-add","mlp", "gp", "mim"])
+    parser.add_argument("--sem_type", default="gp", choices=["gp-add","mlp", "gp", "mim"])
     parser.add_argument("--graph_type", default='ER')
 
 
@@ -23,4 +23,9 @@ def config_parser():
     parser.add_argument('--reweight_epoch', type=int, default=3, help='the epoch begin to reweight')
     parser.add_argument("--w_threshold", default=0.3, type=float)
     parser.add_argument("--data_type", default='real', type=str, help = 'real or synthetic')
+
+
+    # TODO: add the arguments for adapitve reweight
+    # add the batch_size
+    parser.add_argument('--batch_size', type=int, default=1000, help='batch_size')
     return parser
