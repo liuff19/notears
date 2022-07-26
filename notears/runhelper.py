4a7d39dd19338c1c6c7c4e36c21049870e729016
liuff19 <1447243578@qq.com>
2022-07-26 23:01:24 +0800

    runhelper

--- notears/runhelper.py
+++ notears/runhelper.py
@@ -6,15 +6,15 @@ def config_parser():
     parser.add_argument('--config', is_config_file=True,help='config file path')
     parser.add_argument('--device', default='cpu', help='cuda or cpu')
 
-    parser.add_argument("--s0", default=40, type=int)
-    parser.add_argument("--d", default=10, type=int)
+    parser.add_argument("--s0", default=80, type=int)
+    parser.add_argument("--d", default=20, type=int)
     parser.add_argument("--n", default=1000, type=int)
-    parser.add_argument("--sem_type", default="gp", choices=["gp-add","mlp", "gp", "mim"])
-    parser.add_argument("--graph_type", default='SF')
+    parser.add_argument("--sem_type", default="mlp", choices=["gp-add","mlp", "gp", "mim"])
+    parser.add_argument("--graph_type", default='ER')
 
 
     parser.add_argument('--data_dir', type=str, default='data', help='dataset_path')
-    parser.add_argument('--seed', type=int, default=1, help='random seed')
+    parser.add_argument('--seed', type=int, default=0, help='random seed')
 
     parser.add_argument('--lambda1', type=float, default=0.01, help='lambda1')
     parser.add_argument('--lambda2', type=float, default=0.01, help='lambda2')
@@ -22,5 +22,5 @@ def config_parser():
     parser.add_argument('--beta', type=float, default=0.9, help='beta')
     parser.add_argument('--reweight_epoch', type=int, default=3, help='the epoch begin to reweight')
     parser.add_argument("--w_threshold", default=0.3, type=float)
-    parser.add_argument("--data_type", default='synthetic', type=str, help = 'real or synthetic')
+    parser.add_argument("--data_type", default='real', type=str, help = 'real or synthetic')
     return parser
