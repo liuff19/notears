4f9cd95fa918fc20b49eb2d891e54c52d270331d
liuff19 <1447243578@qq.com>
2022-07-27 20:41:33 +0800

    add configuration for reweight

--- notears/runhelper.py
+++ notears/runhelper.py
@@ -9,7 +9,7 @@ def config_parser():
     parser.add_argument("--s0", default=40, type=int)
     parser.add_argument("--d", default=10, type=int)
     parser.add_argument("--n", default=1000, type=int)
-    parser.add_argument("--sem_type", default="gp", choices=["gp-add","mlp", "gp", "mim"])
+    parser.add_argument("--sem_type", default="mlp", choices=["gp-add","mlp", "gp", "mim"])
     parser.add_argument("--graph_type", default='ER')
 
 
@@ -20,12 +20,18 @@ def config_parser():
     parser.add_argument('--lambda2', type=float, default=0.01, help='lambda2')
     parser.add_argument('--reweight', action='store_true', help='if reweight')
     parser.add_argument('--beta', type=float, default=0.9, help='beta')
-    parser.add_argument('--reweight_epoch', type=int, default=3, help='the epoch begin to reweight')
+    
     parser.add_argument("--w_threshold", default=0.3, type=float)
-    parser.add_argument("--data_type", default='real', type=str, help = 'real or synthetic')
+    parser.add_argument("--data_type", default='synthetic', type=str, help = 'real or synthetic')
 
 
     # TODO: add the arguments for adapitve reweight
     # add the batch_size
-    parser.add_argument('--batch_size', type=int, default=1000, help='batch_size')
+    parser.add_argument('--batch_size', type=int, default=200, help='batch_size')
+    parser.add_argument('--reweight_epoch', type=int, default=1, help='the epoch begin to reweight')
+
+    parser.add_argument("--adaptive_epoch", default=500, type=int, help="number of iterations for adaptive reweight")
+    parser.add_argument("--adaptive_lr", default=0.0003, type=float, help="learning rate for adaptive reweight")
+    parser.add_argument("--adaptive_lambda", default=0.01, type=float, help="adaptive lambda for l1 regularization")    
+
     return parser
