c9b29a1d9865ff87366cc580b3dbbb2944927156
liuff19 <1447243578@qq.com>
2022-07-27 16:42:49 +0800

    need to be checked

--- adaptive_model/adapModel.py
+++ adaptive_model/adapModel.py
@@ -11,6 +11,7 @@ class adaptiveMLP(nn.Module):
         super(adaptiveMLP, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size, bias=bias)
         self.fc2 = nn.Linear(hidden_size, output_size, bias=bias)
+        self.sigmoid = nn.Sigmoid()
         # 是否针对relu函数的权重初始化
         nn.init.kaiming_normal_(self.fc1.weight)
         nn.init.kaiming_normal_(self.fc2.weight)
@@ -21,7 +22,7 @@ class adaptiveMLP(nn.Module):
         x = F.relu(self.fc1(x))
         # x = (self.fc2(x)) + tmp
         x = self.fc2(x)
-        x = F.sigmoid(x)
+        x = self.sigmoid(x)
         x = torch.abs(x)
         x = x/torch.sum(x)
         return x
@@ -59,11 +60,15 @@ def adap_reweight_step(adp_model, train_loader, lambda1, notears_model, epoch_nu
             loss.backward()
             optimizer.step()
             loop.set_postfix(adaptive_loss=loss.item())
-            for param in adp_model.fc1.parameters():
-                # 打印梯度
-                print(param.grad)
-        print(reweight_list.squeeze(1))
-        return reweight_list
+            # for param in adp_model.fc1.parameters():
+            #     # 打印梯度
+            #     print(param.grad)
+
+    print(f'平均值：{torch.mean(reweight_list)}')
+    print(f'最大值：{torch.max(reweight_list).item()}')
+    print(f'最小值：{torch.min(reweight_list).item()}')
+        
+    return reweight_list
 
 
 # 测试上述的模型
@@ -86,7 +91,7 @@ if __name__ == '__main__':
     model = adaptiveMLP(input_size=X.shape[1], hidden_size=X.shape[1], output_size=1)
     W_star = torch.randn(10, 10)
     # 调用上述函数，进行模型的训练
-    M = adap_reweight_step(model, train_loader1, lambda1=0.1, W_star=W_star, iter_num=1000, lrate = 0.001)
+    M = adap_reweight_step(model, train_loader1, lambda1=0.1, notears_model=model, epoch_num=1000, lrate = 0.001)
     print("finished")
     # print(M)
     # print(M.sum())
