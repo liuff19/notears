be581262440ba297e41dd2d00307a2977c0a5919
liuff19 <1447243578@qq.com>
2022-07-29 18:47:11 +0800

    run

--- notears/runhelper.py
+++ notears/runhelper.py
@@ -6,8 +6,8 @@ def config_parser():
     parser.add_argument('--config', is_config_file=True,help='config file path')
     parser.add_argument('--device', default='cpu', help='cuda or cpu')
 
-    parser.add_argument("--s0", default=80, type=int)
-    parser.add_argument("--d", default=20, type=int)
+    parser.add_argument("--s0", default=40, type=int)
+    parser.add_argument("--d", default=10, type=int)
     parser.add_argument("--n", default=1000, type=int)
     parser.add_argument("--sem_type", default="gp", choices=["gp-add","mlp", "gp", "mim"])
     parser.add_argument("--graph_type", default='ER')
@@ -22,7 +22,7 @@ def config_parser():
     parser.add_argument('--beta', type=float, default=0.9, help='beta')
     
     parser.add_argument("--w_threshold", default=0.3, type=float)
-    parser.add_argument("--data_type", default='synthetic', type=str, help = 'real or synthetic')
+    parser.add_argument("--data_type", default='real', type=str, help = 'real or synthetic')
 
 
     # TODO: add the arguments for adapitve reweight
@@ -30,7 +30,7 @@ def config_parser():
     parser.add_argument('--batch_size', type=int, default=200, help='batch_size')
     parser.add_argument('--reweight_epoch', type=int, default=1, help='the epoch begin to reweight')
 
-    parser.add_argument("--adaptive_epoch", default=500, type=int, help="number of iterations for adaptive reweight")
+    parser.add_argument("--adaptive_epoch", default=1000, type=int, help="number of iterations for adaptive reweight")
     parser.add_argument("--adaptive_lr", default=0.03, type=float, help="learning rate for adaptive reweight")
     parser.add_argument("--adaptive_lambda", default=0.01, type=float, help="adaptive lambda for l1 regularization")    
 
