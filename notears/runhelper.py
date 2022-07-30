2dc461006a29606f6493721032d03cc91d4d318b
liuff19 <1447243578@qq.com>
2022-07-30 13:39:56 +0800

    runhelper

--- notears/runhelper.py
+++ notears/runhelper.py
@@ -7,7 +7,7 @@ def config_parser():
     parser.add_argument('--device', default='cpu', help='cuda or cpu')
 
     parser.add_argument("--s0", default=40, type=int)
-    parser.add_argument("--d", default=10, type=int)
+    parser.add_argument("--d", default=10, type=int) # 如果是10 下面的n就是1000, 如果是20就是2000
     parser.add_argument("--n", default=1000, type=int)
     parser.add_argument("--sem_type", default="gp", choices=["gp-add","mlp", "gp", "mim"])
     parser.add_argument("--graph_type", default='ER')
@@ -22,16 +22,16 @@ def config_parser():
     parser.add_argument('--beta', type=float, default=0.9, help='beta')
     
     parser.add_argument("--w_threshold", default=0.3, type=float)
-    parser.add_argument("--data_type", default='real', type=str, help = 'real or synthetic')
+    parser.add_argument("--data_type", default='synthetic', type=str, help = 'real or synthetic')
 
 
     # TODO: add the arguments for adapitve reweight
     # add the batch_size
     parser.add_argument('--batch_size', type=int, default=200, help='batch_size')
-    parser.add_argument('--reweight_epoch', type=int, default=1, help='the epoch begin to reweight')
+    parser.add_argument('--reweight_epoch', type=int, default=0, help='the epoch begin to reweight')
 
-    parser.add_argument("--adaptive_epoch", default=1000, type=int, help="number of iterations for adaptive reweight")
-    parser.add_argument("--adaptive_lr", default=0.03, type=float, help="learning rate for adaptive reweight")
-    parser.add_argument("--adaptive_lambda", default=0.01, type=float, help="adaptive lambda for l1 regularization")    
+    parser.add_argument("--adaptive_epoch", default=10, type=int, help="number of iterations for adaptive reweight")
+    parser.add_argument("--adaptive_lr", default=0.0001, type=float, help="learning rate for adaptive reweight")
+    parser.add_argument("--adaptive_lambda", default=0.00000000001, type=float, help="adaptive lambda for l1 regularization")    
 
     return parser
