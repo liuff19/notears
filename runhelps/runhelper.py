d2184123ac136a997bcc2ecb3e01d0d5a0b45ae5
liuff19 <1447243578@qq.com>
2022-08-08 21:47:52 +0800

    runhelper add the wasserstein args

--- runhelps/runhelper.py
+++ runhelps/runhelper.py
@@ -6,8 +6,8 @@ def config_parser():
     parser.add_argument('--config', is_config_file=True,help='config file path')
     parser.add_argument('--device', default='cpu', help='cuda or cpu')
 
-    parser.add_argument("--s0", default=40, type=int)
-    parser.add_argument("--d", default=10, type=int) # 如果是10 下面的n就是1000, 如果是20就是2000, 如果d>50, n最好加大
+    parser.add_argument("--s0", default=80, type=int)
+    parser.add_argument("--d", default=40, type=int) # 如果是10 下面的n就是1000, 如果是20就是2000, 如果d>50, n最好加大
     parser.add_argument("--n", default=1000, type=int)
     parser.add_argument("--sem_type", default="gp", choices=["gp-add","mlp", "gp", "mim"])
     parser.add_argument("--graph_type", default='ER', choices=['SF', 'ER', 'BA'])
@@ -27,12 +27,23 @@ def config_parser():
 
     # TODO: add the arguments for adapitve reweight ， 默认fit好的参数，t = 20, 10, batch=200, adaptive_epoch=10
     # add the batch_size
-    parser.add_argument('--batch_size', type=int, default=200, help='batch_size')
+    parser.add_argument('--batch_size', type=int, default=500, help='batch_size')
     parser.add_argument('--reweight_epoch', type=int, default=0, help='the epoch begin to reweight')
     
     parser.add_argument('--temperature', type=int, default=20, help='softmax_tmperature')
     parser.add_argument("--adaptive_epoch", default=10, type=int, help="number of iterations for adaptive reweight")
     parser.add_argument("--adaptive_lr", default=0.0001, type=float, help="learning rate for adaptive reweight")
     parser.add_argument("--adaptive_lambda", default=0.001, type=float, help="adaptive lambda for l1 regularization")    
-
+    
+    # TODO: add arguments for wasserstein loss
+    parser.add_argument('--run_mode', type = int, default=3, help=' 0: reweight + batch ; 1: baseline; 2: batch + baseline; 3: wasserstein')
+    parser.add_argument('--iter_mod', type= int, default =2, help='the iteration mod for wassertein')
+    parser.add_argument('--clip', type= int, default =0.01, help='the clip for Lipshitz')
+    parser.add_argument('--space_lr', type=int, default =0.01, help='the learning rate of space optimizer')
+    
     return parser
+
+if __name__ == '__main__':
+    parser = config_parser()
+    args = parser.parse_args()
+    print(args)
