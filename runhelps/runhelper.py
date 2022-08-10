d27055427f8e256abef8a6e41143965c17d2536f
liuff19 <1447243578@qq.com>
2022-08-10 14:31:41 +0800

    update

--- runhelps/runhelper.py
+++ runhelps/runhelper.py
@@ -6,11 +6,11 @@ def config_parser():
     parser.add_argument('--config', is_config_file=True,help='config file path')
     parser.add_argument('--device', default='cpu', help='cuda or cpu')
 
-    parser.add_argument("--s0", default=80, type=int)
-    parser.add_argument("--d", default=40, type=int) # 如果是10 下面的n就是1000, 如果是20就是2000, 如果d>50, n最好加大
+    parser.add_argument("--s0", default=60, type=int)
+    parser.add_argument("--d", default=20, type=int) # 如果是10 下面的n就是1000, 如果是20就是2000, 如果d>50, n最好加大
     parser.add_argument("--n", default=1000, type=int)
     parser.add_argument("--sem_type", default="gp", choices=["gp-add","mlp", "gp", "mim"])
-    parser.add_argument("--graph_type", default='SF', choices=['SF', 'ER', 'BA'])
+    parser.add_argument("--graph_type", default='ER', choices=['SF', 'ER', 'BA'])
 
 
     parser.add_argument('--data_dir', type=str, default='data', help='dataset_path')
@@ -22,17 +22,18 @@ def config_parser():
     parser.add_argument('--beta', type=float, default=0.9, help='beta')
     
     parser.add_argument("--w_threshold", default=0.3, type=float)
-    parser.add_argument("--data_type", default='synthetic', type=str, help = 'real or synthetic', choices=['real', 'synthetic','testing', 'sachs7000'])
+    parser.add_argument("--data_type", default='synthetic', type=str, help = 'real or synthetic', choices=['real', 'synthetic','testing', 'sachs_full'])
 
 
     # TODO: add the arguments for adapitve reweight ， 默认fit好的参数，t = 20, 10, batch=200, adaptive_epoch=10
     # add the batch_size
+    parser.add_argument("--run_mode", type = int, default=0, help =" run baseline or reweight operation")
     parser.add_argument('--batch_size', type=int, default=500, help='batch_size')
     parser.add_argument('--reweight_epoch', type=int, default=0, help='the epoch begin to reweight')
     
     parser.add_argument('--temperature', type=int, default=20, help='softmax_tmperature')
     parser.add_argument("--adaptive_epoch", default=10, type=int, help="number of iterations for adaptive reweight")
-    parser.add_argument("--adaptive_lr", default=0.0001, type=float, help="learning rate for adaptive reweight")
+    parser.add_argument("--adaptive_lr", default=0.001, type=float, help="learning rate for adaptive reweight")
     parser.add_argument("--adaptive_lambda", default=0.001, type=float, help="adaptive lambda for l1 regularization")    
 
     return parser
