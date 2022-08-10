8eea76e690f0cbfb0756809144299664985a5cd0
liuff19 <1447243578@qq.com>
2022-08-10 14:31:05 +0800

    create more nodes run file

--- experiment_20.sh
+++ experiment_20.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
-for d in {10,15}
+
+# for d in {20,30,40}
+# do
+#     for multiplier in {2,3,4}
+#     do
+#         for seed in {0..10}
+#         do
+#             for mode in {1,0}
+#             do
+#                 for n in {1000,1500,2000}
+#                 do 
+#                     batch=500
+#                     s0=$(($d*$multiplier))
+#                     sem_type="gp-add"
+#                     graph_type="ER"
+#                     python nonlinear_adp.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed} --batch_size ${batch} --run_mode ${mode} --n ${n}
+#                 done 
+#             done
+#         done
+#     done
+# done
+
+
+for d in {20,30,40}
 do
     for multiplier in {2,3,4}
     do
-        for seed in {0,5,11,17,21}
+        for seed in {0..10}
         do
-            s0=$(($d*$multiplier))
-            sem_type="gp"
-            graph_type="ER"
-            python nonlinear_adp.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed}
+            for mode in {1,0}
+            do
+                for n in {1000,1500,2000}
+                do 
+                    batch=500
+                    s0=$(($d*$multiplier))
+                    sem_type="gp-add"
+                    graph_type="SF"
+                    python nonlinear_adp.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed} --batch_size ${batch} --run_mode ${mode} --n ${n}
+                done 
+            done
         done
     done
 done
 
 
-for d in {10,15}
-do
-    for multiplier in {2,3,4}
-    do
-        for seed in {0,5,11,17,21}
-        do
-            s0=$(($d*$multiplier))
-            sem_type="gp"
-            graph_type="SF"
-            python nonlinear_adp.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed}
-        done
-    done
-done
+
+
+
