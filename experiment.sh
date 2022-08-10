8cb5854aa2dcb3688c9efc51933a8cd5e89a9cb0
liuff19 <1447243578@qq.com>
2022-08-10 14:31:12 +0800

    update

--- experiment.sh
+++ experiment.sh
@@ -6,7 +6,7 @@ do
         for seed in {0,5,11,17,21}
         do
             s0=$(($d*$multiplier))
-            sem_type="gp"
+            sem_type="gp-add"
             graph_type="ER"
             python nonlinear_adp.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed}
         done
@@ -21,7 +21,7 @@ do
         for seed in {0,5,11,17,21}
         do
             s0=$(($d*$multiplier))
-            sem_type="gp"
+            sem_type="gp-add"
             graph_type="SF"
             python nonlinear_adp.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed}
         done
