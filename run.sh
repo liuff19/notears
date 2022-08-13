4ed120d7b1a120b74ef428a08de0c3b5201a77c1
liuff19 <1447243578@qq.com>
2022-08-13 17:47:53 +0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
+for multiplier in {2,3,4}
+do
+    for seed in {0..10}
+    do
+        for mode in {1,3}
+        do
+            n=1000
+            d=10
+            s0=$(($d*$multiplier))
+            sem_type="gp"
+            graph_type="ER"
+            python nonlinear_w.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed} --batch_size ${batch} --run_mode ${mode} --n ${n}
+        done
+    done
+done
 
-for d in {10,20,30,40}
+for multiplier in {2,3,4}
+do
+    for seed in {0..10}
+    do
+        for mode in {1,3}
+        do
+            n=1000
+            d=10
+            s0=$(($d*$multiplier))
+            sem_type="gp"
+            graph_type="SF"
+            python nonlinear_w.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed} --batch_size ${batch} --run_mode ${mode} --n ${n}
+        done
+    done
+done
+
+
+for d in {20,30,40}
 do
     for multiplier in {2,3,4}
     do
@@ -23,7 +55,7 @@ do
     done
 done
 
-for d in {10,20,30,40}
+for d in {20,30,40}
 do
     for multiplier in {2,3,4}
     do
