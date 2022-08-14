a4351c268b701f48f0b52335f1ed845f740aceb5
liuff19 <1447243578@qq.com>
2022-08-14 20:43:11 +0800

    add the 20 nodes

---
+++ run_20.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+for multiplier in {2,3,4}
+do
+    for seed in {0..10}
+    do
+        for mode in {1,3}
+        do
+            for n in {1000,1500,2000}
+            do
+                for batch in {200,500}
+                do 
+                    d=20
+                    s0=$(($d*$multiplier))
+                    sem_type="gp"
+                    graph_type="ER"
+                    python nonlinear_w.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed} --batch_size ${batch} --run_mode ${mode} --n ${n}
+                done
+            done 
+        done
+    done
+done
+
+for multiplier in {2,3,4}
+do
+    for seed in {0..10}
+    do
+        for mode in {1,3}
+        do
+            for n in {1000,1500,2000}
+            do
+                for batch in {200,500}
+                do 
+                    d=20
+                    s0=$(($d*$multiplier))
+                    sem_type="gp"
+                    graph_type="SF"
+                    python nonlinear_w.py --s0 ${s0} --d ${d} --sem_type ${sem_type} --graph_type ${graph_type} --seed ${seed} --batch_size ${batch} --run_mode ${mode} --n ${n}
+                done
+            done 
+        done
+    done
+done
